8a52cfed07e8c1543f2fd829c4df0201f4a9a062
CHETANVAGHELA <chetanvaghela457@gmail.com>
2023-07-03 22:58:19 +0530

    2535 -  Difference Between Element Sum and Digit Sum of an Array

---
+++ src/2535-Difference-Between-Element-Sum-and-Digit-Sum-of-an-Array.java
@@ -0,0 +1,61 @@
+
+// 2535. Difference Between Element Sum and Digit Sum of an Array
+
+// You are given a positive integer array nums.
+
+// The element sum is the sum of all the elements in nums.
+// The digit sum is the sum of all the digits (not necessarily distinct) that appear in nums.
+// Return the absolute difference between the element sum and digit sum of nums.
+
+// Note that the absolute difference between two integers x and y is defined as |x - y|.
+
+// Example 1:
+
+// Input: nums = [1,15,6,3]
+// Output: 9
+// Explanation: 
+// The element sum of nums is 1 + 15 + 6 + 3 = 25.
+// The digit sum of nums is 1 + 1 + 5 + 6 + 3 = 16.
+// The absolute difference between the element sum and digit sum is |25 - 16| = 9.
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: 0
+// Explanation:
+// The element sum of nums is 1 + 2 + 3 + 4 = 10.
+// The digit sum of nums is 1 + 2 + 3 + 4 = 10.
+// The absolute difference between the element sum and digit sum is |10 - 10| = 0.
+
+class DifferenceBetweenSumOfDigit {
+    public static int differenceOfSum(int[] nums) {
+        
+        int firstSum = 0;
+        int secondSum = 0;
+        int k;
+        for(int i =0;i<nums.length;i++) {
+
+            firstSum = firstSum+nums[i];
+            
+           while(nums[i] > 0) {
+
+                k = nums[i] % 10;
+                secondSum = secondSum + k;
+                nums[i] = nums[i] / 10;
+
+            }
+        }
+
+       return Math.abs(firstSum-secondSum);
+        
+
+    } 
+
+    public static void main(String[] args) {
+        
+         int[] first = {1,15,6,3};
+        int[] second = {1,2,3,4};
+        System.out.println(differenceOfSum(first));
+        System.out.println(differenceOfSum(second));
+
+    }
+}
